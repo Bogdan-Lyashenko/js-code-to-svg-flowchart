977dd2103aff778ab475996a7742dc9368849c9a
Bogdan Lyashenko <lyashenkoinc@gmail.com>
2017-11-03 20:41:41 +0100

    Add download for  editor

--- docs/live-editor/index.js
+++ docs/live-editor/index.js
@@ -1,6 +1,7 @@
 (function () {
     const worker = new Worker('./worker.js'),
         svgImage = document.getElementById('svgImage'),
+        downloadFile = document.getElementById('downloadFile'),
         codeEditor = CodeMirror.fromTextArea(document.getElementById('codeEditor'), {
             lineNumbers: true,
             mode:  'javascript',
@@ -10,6 +11,13 @@
     codeEditor.on('change', _.debounce(() => {
         worker.postMessage({ code: codeEditor.getValue() });
     }), 500);
+
+    downloadFile.addEventListener('click', ()=> {
+        const fileName = `flowchart_${(new Date().toString()).replace(/ /g,'_')}.svg`,
+            file = new File([svgImage.innerHTML], fileName, {type: 'image/svg+xml;charset=utf-8'});
+
+        window.saveAs(file, fileName);
+    });
 
     worker.onmessage = function(message) {
         svgImage.innerHTML = message.data.svg;
