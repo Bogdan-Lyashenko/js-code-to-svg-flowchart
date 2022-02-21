83b7bb19d45b16de30cba572700bace2d3a5a9a9
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2022-02-21 12:40:09 +0800

    feat: support optionalChaining

--- src/builder/astParserConfig.js
+++ src/builder/astParserConfig.js
@@ -8,6 +8,7 @@ export default {
         'asyncGenerators',
         'dynamicImport',
         'exportDefaultFrom',
-        'exportNamespaceFrom'
+        'exportNamespaceFrom',
+        'optionalChaining'
     ]
 };
